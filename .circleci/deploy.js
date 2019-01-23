df15d746fe053c097df1fce1489881155213f855
leeci <webrsb@gmail.com>
2019-01-24 07:38:17 +0800

    circle ci

--- .circleci/deploy.js
+++ .circleci/deploy.js
@@ -15,3 +15,10 @@ ftpDeploy.deploy(config, function(err) {
     if (err) console.log(err)
     else console.log('finished');
 });
+
+ftpDeploy.on('uploading', function(data) {
+    console.log(data.filename)
+    data.totalFilesCount;       // total file count being transferred
+    data.transferredFileCount; // number of files transferred
+    data.filename;             // partial path with filename being uploaded
+});
